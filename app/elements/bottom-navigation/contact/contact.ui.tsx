6dcfb740e9f58d02794f48b52a1f57c97f0d1b2b
Mohd Arif Hussain <arif@bigdataworks.com.my>
2025-05-25 00:31:34 +0800

    latest change

--- app/elements/bottom-navigation/contact/contact.ui.tsx
+++ app/elements/bottom-navigation/contact/contact.ui.tsx
@@ -9,6 +9,15 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "@/components/ui/drawer";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { PhoneIcon } from "@heroicons/react/24/outline";
 import { FaWhatsapp } from "react-icons/fa";
@@ -40,39 +49,37 @@ export function ContactDrawer({ open, onOpenChange }: CalendarDrawerInterface) {
         </DrawerHeader>
 
         <ScrollArea className="h-[300px] px-4 pb-6">
-          <div className="space-y-6">
-            {contacts.map((contact, index) => (
-              <div key={index} className={styles.card}>
-                <img
-                  src={getAvatarUrl(contact)}
-                  alt={contact.name}
-                  className="w-16 h-16 rounded-full mx-auto mb-2"
-                />
-                <p className="text-lg font-semibold">{contact.name}</p>
-                <p className="text-gray-500">{contact.designation}</p>
-                <p className="text-gray-500">{contact.phone}</p>
-
-                <div className="flex justify-center gap-4 mt-4">
-                  <a
-                    href={`https://wa.me/${contact.phone.replace("+", "")}`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className={styles.whatsappButton}
-                  >
-                    <FaWhatsapp className="w-5 h-5" />
-                    {translations.whatsappButtonText}
-                  </a>
-                  <a
-                    href={`tel:${contact.phone}`}
-                    className={styles.callButton}
-                  >
-                    <PhoneIcon className="w-5 h-5" />
-                    {translations.callButtonText}
-                  </a>
-                </div>
-              </div>
-            ))}
-          </div>
+          <Table>
+              {/* <TableCaption>A list of your recent invoices.</TableCaption> */}
+              <TableBody>
+                    {contacts.map((contact, index) => (
+                      
+                      <tr key={index} className="hover:bg-muted/50 data-[state=selected]:bg-muted border-b transition-colors">
+                        <td className="font-medium px-2 py-2 sm:w-1/3">
+                          <img src={getAvatarUrl(contact)} alt={contact.name} className="w-16 h-16 rounded-full mx-auto mb-2"/>
+                        </td>
+                        <td className="font-medium px-2 py-2 sm:w-1/3"><a className='text-gray-500'><strong>{contact.name}</strong></a><p className='text-gray-500'>{contact.designation}</p></td>
+                        <td className="px-4 py-2 sm:w-1/3">
+                          <a href={`tel:${contact.phone}`} className={styles.callButton}>
+                            <PhoneIcon className="w-5 h-5" />
+                            {translations.callButtonText}
+                          </a>
+                        </td>
+                        <td className="px-4 py-2 sm:w-1/3">
+                          <a
+                            href={`https://wa.me/${contact.phone.replace("+", "")}`}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className={styles.whatsappButton}
+                          >
+                            <FaWhatsapp className="w-5 h-5" />
+                            {translations.whatsappButtonText}
+                          </a>
+                        </td>
+                      </tr>
+                      ))}
+              </TableBody>
+            </Table>
         </ScrollArea>
 
         <DrawerFooter>
