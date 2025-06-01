d9a546d6c6ca4c37dca30c106888fe62de0cc03d
Amirul Irfan <amirul.irfan.biz@gmail.com>
2025-06-01 16:24:49 +0800

    env config

--- app/config/config-app-server.process.ts
+++ app/config/config-app-server.process.ts
@@ -1,108 +1,95 @@
-'use server'
+"use server";
 import { createClient } from "@supabase/supabase-js";
-import { emailDemoConfig, serverConfig, serverDemoConfig } from "./config-app-environment";
+import {
+  emailDemoConfig,
+  serverConfig,
+  serverDemoConfig,
+} from "./config-app-environment";
 import { EmailConfig, ServerConfig } from "./config-app-environment-interface";
 
 export async function isAppServerStatusOk(): Promise<boolean> {
+  const envStatus = {
+    isOK: await getServerEnvironmentInfoOK(serverConfig),
+    isDemo: await isDemoEnv(serverConfig),
+    isServerConfigOk: await isServerConfigOk(serverConfig),
+  };
 
-  await isDemoEnv()
+  if (!envStatus.isOK || !envStatus.isServerConfigOk) {
+    return false;
+  }
 
-  return getServerEnvironmentInfoOK() 
-  
+  if (envStatus.isDemo) {
+    return true;
+  }
+
+  return true;
 }
 
+async function getServerEnvironmentInfoOK(
+  serverConfig: ServerConfig
+): Promise<boolean> {
+  const supabase = createClient(
+    serverConfig.supabaseKey,
+    serverConfig.supabaseAnonKey
+  );
 
-
-  async function getServerEnvironmentInfoOK():Promise<boolean> {
-  const supabase = createClient(serverConfig.supabaseKey, serverConfig.supabaseAnonKey);
-  
   try {
     const { data, error } = await supabase.functions.invoke("environment-info");
-    
+
     if (error) throw error;
-    console.log(data)
-    return true
-    
+    console.log(data);
+    return true;
   } catch (error) {
     console.log({
       status: "offline",
       message: "Could not verify environment",
-      isFallback: true
-    })
-    return false
+      isFallback: true,
+    });
+    return false;
   }
-
 }
 
-async function isDemoEnv(): Promise<boolean>{
-  const isDemoEnv = JSON.stringify(serverConfig) === JSON.stringify(serverDemoConfig) 
-  console.log(serverConfig)
-  isDemoEnv?  console.log('environment using demo env') : console.log('App is live env')
-  return isDemoEnv
+async function isDemoEnv(serverConfig: ServerConfig): Promise<boolean> {
+  const isDemoEnv =
+    JSON.stringify(serverConfig) === JSON.stringify(serverDemoConfig);
+  isDemoEnv
+    ? console.log({
+      environmentDescription: "App is using demo environment config"
+    })
+    : console.log({
+      environmentDescription: "App is using live environment config"
+    });
+  return isDemoEnv;
 }
 
+async function isServerConfigOk(config: ServerConfig): Promise<boolean> {
+  try {
+    const supabase = createClient(config.supabaseKey, config.supabaseAnonKey);
 
-// export class ServerProcessConfig {
-//   async isAppStatusOk(serverConfig: ServerConfig): Promise<boolean> {
-//     await this.getServerEnvironmentInfo(serverConfig);
-//     switch (true) {
-//       case !(await this.isServerConfigOk(serverConfig)):
-//         return false;
+    const { data, error } = await supabase
+      .from(config.rsvpTableName)
+      .select("*")
+      .limit(1);
 
-//       default:
-//         if (this.isEmailDemo(emailDemoConfig)) {
-//           console.warn(
-//             "⚠️ Email config is still using demo values even though the server is running live. Be sure to update before handing to client"
-//           );
-//         }
+    if (error) {
+      console.error({
+        serverDbConfigStatus: "Error",
+        serverDbConfigMessage: `❌ Supabase config validation failed:${error.message}`,
+      });
+      return false;
+    }
 
-//         return true;
-//     }
-//   }
+    console.log({
+      serverDbConfigStatus: "Success",
+      serverDbConfigMessage: "server config Ok",
+    });
 
-//   private isEmailDemo(config: EmailConfig): boolean {
-//     return JSON.stringify(config) === JSON.stringify(emailDemoConfig);
-//   }
-
-  
-//   private async isServerConfigOk(config: ServerConfig): Promise<boolean> {
-//     try {
-//       const supabase = createClient(config.supabaseKey, config.supabaseAnonKey);
-
-//       const { data, error } = await supabase
-//         .from(config.rsvpTableName)
-//         .select("*")
-//         .limit(1);
-
-//       if (error) {
-//         console.error("❌ Supabase config validation failed:", error.message);
-//         return false;
-//       }
-
-//       return true;
-//     } catch (e: any) {
-//       console.error("❌ Supabase connection error:", e.message);
-//       return false;
-//     }
-//   }
-
-//  private async getServerEnvironmentInfo(config: ServerConfig) {
-//   const supabase = createClient(config.supabaseKey, config.supabaseAnonKey);
-  
-//   try {
-//     const { data, error } = await supabase.functions.invoke("environment-info");
-    
-//     if (error) throw error;
-//     console.log("Environment status:", data);
-//     return data;
-    
-//   } catch (error) {
-//     console.error("Check failed:", error);
-//     return {
-//       status: "offline",
-//       message: "Could not verify environment",
-//       isFallback: true
-//     };
-//   }
-// }
-// }
+    return true;
+  } catch (error: any) {
+    console.error({
+      serverDbConfigStatus: "Error",
+      serverDbConfigMessage: `❌ Supabase config validation failed:${error.message}`,
+    });
+    return false;
+  }
+}
