872854937055d1ae030786414fea5a7e0e5d1ab8
Amirul Irfan <amirul.irfan.biz@gmail.com>
2025-10-22 14:13:54 +0800

    Update config-app-environment.ts

--- app/config/config-app-environment.ts
+++ app/config/config-app-environment.ts
@@ -224,8 +224,8 @@ export const moneyGiftConfig: MoneyGiftConfig = {
  */
 export const locationConfig: LocationConfig = {
   coordinates: {
-    latitude: 4.660436990351113, //TODO
-    longitude: 101.07886714814849, //TODO
+    latitude: 4.631103147028483, //TODO
+    longitude: 101.07930715502899, //TODO
   },
   locationInfo: {
     title: "Lokasi Kami",
