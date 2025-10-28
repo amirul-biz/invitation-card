0d01e8c1a0b9a03095cf9c1a432047b3394c5528
Amirul Irfan <amirul.irfan.biz@gmail.com>
2025-10-28 17:17:37 +0800

    Update config-app-environment.ts

--- app/config/config-app-environment.ts
+++ app/config/config-app-environment.ts
@@ -91,7 +91,7 @@ export const emailConfig: EmailConfig = {
 export const weddingCountdownConfig: CountdownConfig = {
   event: {
     name: "Majlis Perkahwinan",
-    date: "2025-09-20T03:00:00Z", //TODO
+    date: "2027-09-20T03:00:00Z", //TODO
     timeZone: "Asia/Kuala_Lumpur",
     location: "Sebening Embun Garden Event Hall", //TODO
   },
@@ -187,8 +187,8 @@ export const weddingCalendarConfig: CalendarDrawerConfig = {
     title: weddingCountdownConfig.ui.title,
     description: "",
     location: weddingCountdownConfig.event.location,
-    startDate: "2025-09-20T03:00:00Z", // 11:00 AM MYT is 03:00 AM UTC //TODO
-    endDate: "2025-09-20T08:00:00Z", // 04:00 PM MYT is 08:00 AM UTC //TODO
+    startDate: "2027-09-20T03:00:00Z", // 11:00 AM MYT is 03:00 AM UTC //TODO
+    endDate: "2027-09-20T08:00:00Z", // 04:00 PM MYT is 08:00 AM UTC //TODO
     timeZone: "Asia/Kuala_Lumpur",
   },
 };
@@ -267,17 +267,7 @@ export const contactConfig: ContactConfig = {
       name: "Nur Izhatie Aisyah",
       phone: "+60123611306",
       designation: "Pengantin Perempuan",
-    },
-    {
-      name: "Juana Binti Ahmad",
-      phone: "+60193823494",
-      designation: "Ibu Pengantin Lelaki",
-    },
-    {
-      name: "Khairul Azreem",
-      phone: "+60196643494",
-      designation: "Bapa Pengantin Lelaki",
-    },
+    }
   ],
   translations: {
     title: "Hubungi Kami",
